dfd1146306c423a5345d54fa9b46b85b6dc8da68
Tyler Hunt <tyler@tylerhunt.com>
2026-07-15 15:49:22 -0400

    Simplify `Bridge` by subclassing `Module`
    Make `Bridge` a `Module` subclass so it can be included directly,
    removing the intermediate `Data` value object, its `module` method, and
    the `tap` over an anonymous module. Private builders now receive the
    machine class and name lambda as parameters, so `define_method` blocks
    close over locals naturally instead of copying attributes into locals
    first.
    
    Also build the machine fresh on each access instead of memoizing it in
    an instance variable. The cached machine went stale whenever the state
    attribute changed underneath it (e.g. `reload` or direct assignment),
    answering predicates from the old state.

--- lib/kintide/state_machine/bridge.rb
+++ lib/kintide/state_machine/bridge.rb
@@ -1,81 +1,63 @@
 module Kintide
   module StateMachine
-    class Bridge < Data.define(:machine_class, :prefix)
-      def self.[](machine_class, prefix: nil)
-        new(machine_class:, prefix:).module
-      end
+    class Bridge < Module
+      def self.[](machine_class, **) = new(machine_class, **)
 
-      def module
-        Module.new.tap { |methods_module|
-          define_machine_accessor methods_module
-          define_event_predicate_methods methods_module
-          define_state_predicate_methods methods_module
-          define_bang_methods methods_module
-          define_invoke_method methods_module
-        }
+      def initialize(machine_class, prefix: nil)
+        super()
+
+        method_name = ->(part) { [*prefix, part].join('_').to_sym }
+
+        define_machine_method machine_class, method_name
+        define_invoke_method method_name
+        define_event_methods machine_class, method_name
+        define_state_predicate_methods machine_class, method_name
       end
 
     private
 
-      def build_method(name) = [*prefix, name].join('_').to_sym
-      def machine_accessor = build_method(:machine)
-      def state_attribute = build_method(:state)
-      def invoke_method = build_method(:invoke)
+      def define_machine_method(machine_class, method_name)
+        state_attribute = method_name[:state]
 
-      def define_machine_accessor(object)
-        variable = :"@#{machine_accessor}"
-        machine_class = self.machine_class
-        state_attribute = self.state_attribute
-
-        object.define_method machine_accessor do
-          instance_variable_get(variable) or
-            instance_variable_set(
-              variable,
-              machine_class.new(state: send(state_attribute)),
-            )
+        define_method method_name[:machine] do
+          machine_class.new(state: send(state_attribute))
         end
       end
 
-      def define_event_predicate_methods(object)
-        machine_accessor = self.machine_accessor
+      def define_invoke_method(method_name)
+        machine_method = method_name[:machine]
+        state_attribute = method_name[:state]
+
+        define_method method_name[:invoke] do |name, context: self|
+          machine = send(machine_method)
+          machine.invoke(name, context:)
+          send :"#{state_attribute}=", machine.state
+        end
+      end
+
+      def define_event_methods(machine_class, method_name)
+        machine_method = method_name[:machine]
+        invoke_method = method_name[:invoke]
 
         machine_class.events.each do |event|
-          object.define_method "can_#{build_method(event)}?" do |context: self|
-            machine = send(machine_accessor)
-            machine.can_invoke?(event, context:)
+          define_method :"can_#{method_name[event]}?" do |context: self|
+            send(machine_method).can_invoke?(event, context:)
           end
-        end
-      end
 
-      def define_state_predicate_methods(object)
-        state_attribute = self.state_attribute
-
-        machine_class.states.each_key do |state|
-          object.define_method "#{build_method(state)}?" do
-            send(state_attribute) == state
-          end
-        end
-      end
-
-      def define_bang_methods(object)
-        invoke_method = self.invoke_method
-
-        machine_class.events.each do |event|
-          object.define_method "#{build_method(event)}!" do |context: self|
+          define_method :"#{method_name[event]}!" do |context: self|
             send(invoke_method, event, context:)
             save!
           end
         end
       end
 
-      def define_invoke_method(object)
-        machine_accessor = self.machine_accessor
-        state_attribute = self.state_attribute
+      def define_state_predicate_methods(machine_class, method_name)
+        state_attribute = method_name[:state]
 
-        object.define_method invoke_method do |name, context: self|
-          machine = send(machine_accessor)
-          machine.invoke(name, context:)
-          send :"#{state_attribute}=", machine.state
+        machine_class.states.each_key do |state|
+          define_method :"#{method_name[state]}?" do
+            send(state_attribute) == state
+          end
         end
       end
     end
